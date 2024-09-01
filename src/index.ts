dd0d494c5fe6753933da44d429ba061d039aed69
nowaaru@proton.me <vert@leanbox.>
2024-09-01 10:55:23 +0000

    remove ugly imports
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import type Flamework from "@flamework/core";
 
-import { CombatService    } from "services/combat.service";
-import { EffectsService   } from "services/effects.service";
-import { MatchService     } from "services/matchservice.service";
-
-import { MovementService  } from "services/movement.service";
-import { QuarrelGame      } from "services/quarrelgame.service";
-import { ResolverService  } from "services/resolver.service";
-import { SchedulerService } from "services/scheduler.service";
-import { CommandService   } from "services/cmdr.service";
-
 export * from "services/cmdr.service";
 export * from "services/combat.service";
 export * from "services/effects.service";
